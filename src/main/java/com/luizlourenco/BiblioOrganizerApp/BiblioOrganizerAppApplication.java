7e44c3c12f0398f138a40aa3419f87343b0557fb
Luiz Lourenco <luiz.lourenco@dhl.com>
2025-09-08 02:53:05 -0300

    Inicializando repositório com novos arquivos

---
+++ src/main/java/com/luizlourenco/BiblioOrganizerApp/BiblioOrganizerAppApplication.java
@@ -0,0 +1,13 @@
+package com.luizlourenco.BiblioOrganizerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioOrganizerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioOrganizerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizlourenco/BiblioOrganizerApp/BiblioOrganizerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizlourenco.BiblioOrganizerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioOrganizerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
